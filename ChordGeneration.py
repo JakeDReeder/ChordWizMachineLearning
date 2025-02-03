d4c03d47f4a591abc8bf94f3ed7423c33d788278
Jake Reeder <jacobdreeder@gmail.com>
2025-02-02 20:52:02 -0500

    Made methods for generating data.

--- ChordGeneration.py
+++ ChordGeneration.py
@@ -4,7 +4,7 @@
 import librosa
 import os
 import mido
-from mido import Message, Midifile, MidiTrack
+from mido import Message, MidiFile, MidiTrack
 import subprocess
 
 # List of arrays for chords 
@@ -15,8 +15,8 @@ CHORDS = {
 INSTRUMENTS = {"piano": 0 }
 
 # Method for making MIDI file
-def Make_MIDI_file(chord_name, notes, instrument, filename):
-    midi = Midifile()
+def make_midi_file(chord_name, notes, instrument, filename):
+    midi = MidiFile()
     track = MidiTrack()
     midi.tracks.append(track)
 
@@ -27,17 +27,32 @@ def Make_MIDI_file(chord_name, notes, instrument, filename):
     for note in librosa.note_to_midi(notes):
         track.append(Message('note_on', note=int(note), velocity=64, time=0))
 
-    # sustain the note for 5 seconds
-    track.append(Message('note_off', note=int(librosa.note_to_midi(notes)[0]), velocity=64, time=5000))
+    # sustain the note for 4 seconds
+    track.append(Message('note_off', note=int(librosa.note_to_midi(notes)[0]), velocity=64, time=4000))
 
     #stop midi input
     for note in librosa.note_to_midi(notes[1:]):
         track.append(Message('note_off', note=int(note), velocity=64, time=0))
 
     # save the midifile
-    midi_path = f"dataset/midi/{filenname}.mid"
+    midi_path = f"dataset/midi/{filename}.mid"
     midi.save(midi_path)
     return midi_path
 
+def convert_midi_to_wav(midi_path, wav_path, soundfont="soundfont.sf2"):
+    command = ["fluidsynth", "-ni", soundfont, midi_path, "-F", wav_path, "-r", "44100"] # fluidsynth command to convert to wav 
+    subprocess.run(command, check=True)
 
-print(Make_MIDI_Object(chords))
+def generate_chord_dataset():
+    for chord_name, inversions in CHORDS.items():
+        for inversion in inversions:
+            for instrument in INSTRUMENTS.keys():
+                filename = f"{chord_name}_{'_'.join(inversion)}_{instrument}"
+                midi_path = make_midi_file(chord_name, inversion, instrument, filename)
+                wav_path = f"dataset/wav/{filename}.wav"
+
+                convert_midi_to_wav(midi_path, wav_path)
+                print(f"Generated: {wav_path}")
+
+
+generate_chord_dataset();
